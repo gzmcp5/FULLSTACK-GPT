45172a919363b2e308feb2ad63684eb8a93a63b3
gzmcp <jigil0925@gmail.com>
2026-04-21 08:03:08 +0900

    6. RAG

--- scratch2.py
+++
@@ -1,6 +0,0 @@
-from langchain_unstructured import UnstructuredLoader
-loader = UnstructuredLoader("./files/chapter_one.pdf", mode="single")
-docs = loader.load()
-print("docs len:", len(docs))
-print("text len:", len(docs[0].page_content))
-print(repr(docs[0].page_content[:200]))
